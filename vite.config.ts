acf1c0a526531d7d539dd83e1d4183ab5586fa3c
Cz ElEcTrolivE <khoy2589@gmail.com>
2025-06-14 13:44:15 +0700

    remove unknown import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 
 // https://vitejs.dev/config/
@@ -14,8 +13,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
-    mode === 'development' &&
-    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
